0c816342ecf203a29095fd7388f959b9ce5e41ad
Ly Duong <ly.duong@metropolia.fi>
2019-01-21 07:31:27 +0000

    finished exercise xxxx

--- w1 JavaScript Language/01 JavaScript Tools/js/calculations.js
+++ w1 JavaScript Language/01 JavaScript Tools/js/calculations.js
@@ -1,12 +1,27 @@
+
+
 function multiplyBy()
 {
 // check the input
-    
+var first = document.getElementById("firstNumber").value;
+var second = document.getElementById("secondNumber").value;
+var answer = parseFloat(first) * parseFloat(second);
+if(!isNaN(answer))
+{
+	document.getElementById("result").innerHTML=answer;
+}
 }
 
-function divideBy() 
-{ 
+function divideBy(f,s)
+{
 // check the input
- 
+var first = document.getElementById("firstNumber").value;
+var second = document.getElementById("secondNumber").value;
+var answer = parseFloat(first) / parseFloat(second);
+if(!isNaN(answer))
+{
+	document.getElementById("result").innerHTML=answer;
+}
+
 }
 
--- w1 JavaScript Language/01 JavaScript Tools/js/guess.js
+++ w1 JavaScript Language/01 JavaScript Tools/js/guess.js
@@ -1,23 +1,44 @@
-let targetNumber = Math.floor(Math.random() * 10) + 1;
-
+"use strict";
+var targetNumber = Math.floor(Math.random() * 10) + 1;
+var count = 0;
+const number = document.querySelector('.number');
 function init () {
 	console.log("You have to code this!");
 }
 
-function check (value) {
-  
+function check()
+{
+	var win = false;
+	while (parseFloat(count) <= parseFloat(5) && win == false)
+  {
+  	count = parseFloat(count)+1;
+		if (parseFloat(number)==parseFloat(targetNumber))
+  	{
+  		win = true;
+		} else
+		{
+			showError();
+		}
+	}
+	if(win == true)
+	{
+		showWin();
+	}else{
+	showLoss();
+	}
 }
 
 function showWin () {
-  
+  console.log("You won!");
+  console.log(targetNumber);
 }
 
 function showError () {
-  
+  console.log("Error");
+  console.log(targetNumber);
 }
 
 function showLoss () {
-  
+	console.log(targetNumber);
+	console.log("You lose!");
 }
-
-init();
